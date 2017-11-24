1eb0aa98f7ebcaf129d444c78a128c717c5ec25c
yang wang <yangwang@yangdemacbook-pro.local>
2017-11-24 14:06:55 +0800

    Initial Commit

---
+++ LearnSceneKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnSceneKit
+//
+//  Created by yang wang on 2017/11/24.
+//  Copyright © 2017年 yang wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
