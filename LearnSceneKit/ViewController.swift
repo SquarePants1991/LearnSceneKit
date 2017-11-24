85c902b3d0eb0ce3b6708ada9d175038a4a29eb5
yang wang <yangwang@yangdeMacBook-Pro.local>
2017-11-24 16:29:34 +0800

    init

--- LearnSceneKit/ViewController.swift
+++ LearnSceneKit/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
+    var scene: SCNScene!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Create & Config Scene
+        scene = SCNScene()
+        scene.background.contents = UIColor.black
+        createCamera()
+        createLight()
+        createGeometry()
+
+        // Config SCNView
+        if let scnView = self.view as? SCNView {
+            scnView.scene = scene
+
+            scnView.allowsCameraControl = true
+            scnView.showsStatistics = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createCamera() {
+        let node = SCNNode()
+        node.camera = SCNCamera()
+        node.position = SCNVector3.init(0, 0, 2)
+        scene.rootNode.addChildNode(node)
     }
 
+    func createLight() {
+        // red light
+        let redLightNode = SCNNode()
+        redLightNode.light = SCNLight()
+        redLightNode.light?.type = .omni
+        redLightNode.light?.color = UIColor.red
+        redLightNode.position = SCNVector3.init(6, 6, 0)
+        scene.rootNode.addChildNode(redLightNode)
 
+        // green light
+        let greenLightNode = SCNNode()
+        greenLightNode.light = SCNLight()
+        greenLightNode.light?.type = .omni
+        greenLightNode.light?.color = UIColor.green
+        greenLightNode.position = SCNVector3.init(-6, 6, 0)
+        scene.rootNode.addChildNode(greenLightNode)
+
+        // blue light
+        let blueLightNode = SCNNode()
+        blueLightNode.light = SCNLight()
+        blueLightNode.light?.type = .omni
+        blueLightNode.light?.color = UIColor.blue
+        blueLightNode.position = SCNVector3.init(0, -6, 0)
+        scene.rootNode.addChildNode(blueLightNode)
+    }
+
+    func createGeometry() {
+        let node = SCNNode()
+        node.geometry = SCNSphere.init(radius: 0.3)
+        node.position = SCNVector3.init(0, 0, 0)
+        scene.rootNode.addChildNode(node)
+    }
 }
 
